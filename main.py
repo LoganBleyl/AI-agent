bb92828b2c29c869a88ad5c4354f4e4a3a76f4c0
Logan Bleyl <loganmbleyl05@gmail.com>
2025-12-22 20:52:42 -0500

    first

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ response = client.models.generate_content(
         model='gemini-2.5-flash',
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 print(response.text)
+
