1dc684e7348e6ffda2ac320b0445a8ba5814d5d8
Logan Bleyl <loganmbleyl05@gmail.com>
2025-12-24 22:38:40 -0500

    user_prompts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 load_dotenv()
@@ -9,11 +10,27 @@ if api_key is None:
     raise RuntimeError("no api found")
 
 client = genai.Client(api_key=api_key)
+parser = argparse.ArgumentParser(description="AI Agent")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 
 
 response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(response.text)
+        contents= args.user_prompt
+                )
 
+
+
+meta_data = response.usage_metadata
+
+if meta_data is None:
+    raise RuntimeError("metadata not found")
+
+prompt_token_count = meta_data.prompt_token_count
+candidates_token_count = meta_data.candidates_token_count
+
+print(f"Prompt tokens: {prompt_token_count}")
+print(f"Response tokens: {candidates_token_count}")
+print(response.text)
